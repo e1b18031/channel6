a23a738d8ef6e5da0ba5a68237f796fe0d5846ba
e1b18031 <e1b18031@st.oit.ac.jp>
2020-11-10 15:12:26 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/chang6/channel6/Channel6Application.java
@@ -0,0 +1,13 @@
+package oit.is.chang6.channel6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Channel6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Channel6Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/chang6/channel6/Channel6ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.chang6.channel6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Channel6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
