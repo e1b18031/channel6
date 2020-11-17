38df836861d32a368af08bff3d2f214369b6bc1d
Natsuki Takemori <e1b18049@st.oit.ac.jp>
2020-11-17 16:11:57 +0900

    登録ユーザの表示

--- src/main/java/oit/is/chang6/channel6/controller/controller.java
+++ src/main/java/oit/is/chang6/channel6/controller/controller.java
@@ -1,5 +1,8 @@
 package oit.is.chang6.channel6.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +11,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import oit.is.chang6.channel6.model.Users;
+import oit.is.chang6.channel6.model.UsersMapper;
 @Controller
 @RequestMapping("/ch6")
 public class controller {
+  @Autowired
+  UsersMapper usersMapper;
 
   @GetMapping
-  public String ch6() {
+  public String ch6(ModelMap model) {
+    ArrayList<Users> Users1 = usersMapper.selectAll();
+    model.addAttribute("users1",Users1);
     return "ch6.html";
   }
-
 }
